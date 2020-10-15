35e9f5fbd05c9e4c080685f92d7d9c4aad91d5b3
eriudo <erildojunior@live.com>
2020-10-15 19:33:13 -0300

    refactor javascript

--- introduction/challenge/index.js
+++ introduction/challenge/index.js
@@ -1,14 +1,16 @@
 import Vue from "https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.esm.browser.js";
 Vue.component("todo-item", {
-  props: ["todo", "cancel", "done", "erase", "index", "style"],
-  template: `<li> 
-    {{ todo.text }} 
+  props: ["cancel", "done", "erase", "item", "index"],
+  template: `
+  <li> 
+    {{ item.text }} 
     <span class="control-buttons">
-    <button v-on:click="done(index)" type="button" class="material-icons done-button"> done </button> 
-    <button v-on:click="cancel(index)" type="button" class="material-icons cancel-button"> clear </button>
+    <button v-on:click="done(item)" type="button" class="material-icons done-button"> done </button> 
+    <button v-on:click="cancel(item)" type="button" class="material-icons cancel-button"> clear </button>
     <button v-on:click="erase(index)" class="material-icons delete-button"> delete </button>
     </span>
-     </li>`,
+  </li>
+     `,
 });
 
 window.app = new Vue({
@@ -23,18 +25,27 @@ window.app = new Vue({
       this.lista.push({
         id: this.idTracker++,
         text: this.message,
-        style: "background-color: rgba(250, 250, 99, 0.747)",
+        status: "new",
       });
       this.message = "";
     },
     erase: function (index) {
       this.lista.splice(index, 1);
     },
-    done: function (index) {
-      this.lista[index].style = "background-color: rgba(193, 243, 193, 0.747);";
+    done: function (item) {
+      item.status = "done";
     },
-    cancel: function (index) {
-      this.lista[index].style = "background-color: rgba(243, 117, 117, 0.747)";
+    cancel: function (item) {
+      item.status = "overdue";
+    },
+    getTodoStyle: function (status) {
+      if (status == "new") {
+        return "background-color: rgba(250, 250, 99, 0.747)";
+      }
+      if (status == "done") {
+        return "background-color: rgba(193, 243, 193, 0.747)";
+      }
+      return "background-color: rgba(243, 117, 117, 0.747)";
     },
   },
 });
