54ff1b244db2bfefd775537ba19a55168fd0d810
eriudo <erildojunior@live.com>
2020-10-28 19:36:12 -0300

    add example from computed property

---
+++ computedProperties/index.js
@@ -0,0 +1,15 @@
+import Vue from "https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.esm.browser.js";
+
+window.vm = new Vue({
+  el: "#example",
+  data: {
+    message: "Hello",
+  },
+  computed: {
+    // a computed getter
+    reversedMessage: function () {
+      // `this` points to the vm instance
+      return this.message.split("").reverse().join("");
+    },
+  },
+});
